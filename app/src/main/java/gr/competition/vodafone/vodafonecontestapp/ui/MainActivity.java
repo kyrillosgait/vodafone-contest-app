3738c65bdc4e6ca5b77b64ee2c1542779812d415
Panos Gr <nosgr94@gmail.com>
2018-12-12 14:05:42 +0200

    MainScreen Template

--- app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/MainActivity.java
+++ app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/MainActivity.java
@@ -1,15 +1,33 @@
 package gr.competition.vodafone.vodafonecontestapp.ui;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import gr.competition.vodafone.vodafonecontestapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_play)
+    Button btnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
