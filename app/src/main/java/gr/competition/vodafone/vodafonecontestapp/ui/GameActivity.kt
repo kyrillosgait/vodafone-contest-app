36c730e956e9d5debf528de5d6270bf7fe70c4e9
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2018-12-12 17:29:12 +0200

    Improved game logic

--- app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/GameActivity.kt
+++ app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/GameActivity.kt
@@ -3,13 +3,43 @@ package gr.competition.vodafone.vodafonecontestapp.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import gr.competition.vodafone.vodafonecontestapp.R
+import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
+
+    var selectedBox: Int = 0
+    var boxes = mutableSetOf(1, 2, 3, 4, 5, 6)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        firstBox.setOnClickListener { selectBox(1) }
+        secondBox.setOnClickListener { selectBox(2) }
+        thirdBox.setOnClickListener { selectBox(3) }
+        fourthBox.setOnClickListener { selectBox(4) }
+        fifthBox.setOnClickListener { selectBox(5) }
+        sixthBox.setOnClickListener { selectBox(6) }
     }
+
+    private fun selectBox(boxNumber: Int) {
+        if (boxes.size == 6) {
+            selectedBox = boxNumber
+            boxes.remove(boxNumber)
+            when (boxNumber) {
+                1 -> firstBox.isEnabled = false
+                2 -> secondBox.isEnabled = false
+                3 -> thirdBox.isEnabled = false
+                4 -> fourthBox.isEnabled = false
+                5 -> fifthBox.isEnabled = false
+                6 -> sixthBox.isEnabled = false
+            }
+
+        } else {
+
+        }
+    }
+
 
 }
 
--- app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/MainActivity.java
+++ app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/MainActivity.java
@@ -3,7 +3,6 @@ package gr.competition.vodafone.vodafonecontestapp.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,12 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        btnPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
-                startActivity(intent);
-            }
+        btnPlay.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), GameActivity.class);
+            startActivity(intent);
         });
     }
 }
