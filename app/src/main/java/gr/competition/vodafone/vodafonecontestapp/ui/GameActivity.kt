e4d3842981f251319941edf5502f9bde4e18823d
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2018-12-13 13:21:43 +0200

    GameActivity: StrikeThrough TextViews

--- app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/GameActivity.kt
+++ app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/GameActivity.kt
@@ -1,6 +1,8 @@
 package gr.competition.vodafone.vodafonecontestapp.ui
 
+import android.graphics.Paint
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import gr.competition.vodafone.vodafonecontestapp.R
 import gr.competition.vodafone.vodafonecontestapp.db.AppDatabase
@@ -8,17 +10,18 @@ import gr.competition.vodafone.vodafonecontestapp.db.GiftDao
 import gr.competition.vodafone.vodafonecontestapp.model.Box
 import kotlinx.android.synthetic.main.activity_game.*
 
+
 class GameActivity : AppCompatActivity() {
 
     private var selectedBox: Int = 0
-    private var boxes = mutableSetOf(1, 2, 3, 4, 5, 6)
     private lateinit var giftDao: GiftDao
+    private var boxes = mutableSetOf(1, 2, 3, 4, 5, 6)
+    private var boxesList = mutableListOf<Box>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
         giftDao = AppDatabase.getInstance(this).giftDao()
-//        database = AppDatabase.getInstance(this)
 
         selectRandomGifts()
 
@@ -32,8 +35,7 @@ class GameActivity : AppCompatActivity() {
 
     private fun selectRandomGifts() {
 
-        var boxesSet = boxes
-        var boxesList = mutableListOf<Box>()
+        var boxesSet = mutableSetOf(1, 2, 3, 4, 5, 6)
 
         // Add boxes
         for (i in 1..6) {
@@ -47,13 +49,25 @@ class GameActivity : AppCompatActivity() {
             for (box in boxesList) {
                 if (box.id == randomBox) {
                     box.name = giftDao.selectRandomGiftFromCategory(i)
+                    changeTextViewNameToGift(i, box.name)
                 }
             }
             boxesSet.remove(randomBox)
         }
     }
 
+    private fun changeTextViewNameToGift(textViewNumber: Int, name: String) {
+        when (textViewNumber) {
+            1 -> firstRewardTextView.text = name
+            2 -> secondRewardTextView.text = name
+            3 -> thirdRewardTextView.text = name
+            4 -> fourthRewardTextView.text = name
+        }
+    }
+
     private fun onBoxClicked(boxNumber: Int) {
+        Log.d("BOXES", "BOXES" + boxes.toString())
+
         if (selectedBox == 0) {
             selectedBox = boxNumber
             boxes.remove(boxNumber)
@@ -65,7 +79,25 @@ class GameActivity : AppCompatActivity() {
 
         } else {
 
+            for (box in boxesList) {
+
+                if (box.id == boxNumber) {
+                    strikeThroughReward(box.name)
+                    disableBox(boxNumber)
+                }
+
+            }
         }
+    }
+
+    private fun strikeThroughReward(name: String) {
+        when (name) {
+            firstRewardTextView.text -> firstRewardTextView.paintFlags = firstRewardTextView.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            secondRewardTextView.text -> secondRewardTextView.paintFlags = secondRewardTextView.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            thirdRewardTextView.text -> thirdRewardTextView.paintFlags = thirdRewardTextView.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            fourthRewardTextView.text -> fourthRewardTextView.paintFlags = fourthRewardTextView.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+        }
+
     }
 
     private fun changeBoxNumberTitleToQuestionMark(box: Int) {
