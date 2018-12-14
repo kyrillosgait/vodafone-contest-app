79a0ca226826f4eba5cd05f5935e6bcc1fd88c62
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2018-12-14 13:17:56 +0200

    New boxes for GameActivity and animate Main Activity

--- app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/RewardsAdapter.java
+++ app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/RewardsAdapter.java
@@ -1,7 +1,6 @@
 package gr.competition.vodafone.vodafonecontestapp.ui;
 
 import android.content.Context;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,34 +8,32 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import gr.competition.vodafone.vodafonecontestapp.R;
 import gr.competition.vodafone.vodafonecontestapp.model.Gift;
 
-public class RewardsAdapter extends ArrayAdapter<Gift>
-{
-    public RewardsAdapter(Context context, ArrayList<Gift> list)
-    {
-        super(context,0,list);
+public class RewardsAdapter extends ArrayAdapter<Gift> {
+    public RewardsAdapter(Context context, ArrayList<Gift> list) {
+        super(context, 0, list);
     }
 
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
+    public View getView(int position, View convertView, ViewGroup parent) {
         Gift gift = getItem(position);
 
         if (convertView == null) {
-
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_reward_2, parent, false);
-
         }
 
         TextView tvName = (TextView) convertView.findViewById(R.id.giftName);
 
+        tvName.setText(Objects.requireNonNull(gift).getName());
+//        switch (gift.getCategoryId()) {
+//            case 1: tvName.setCompoundDrawables();
+//        }
 
-        tvName.setText(gift.getName());
         return convertView;
     }
-
 
 
 }
