c3721799cc21f327e5b2ff4d22ac94964c18a665
Panos Gr <nosgr94@gmail.com>
2018-12-14 11:50:21 +0200

    Retries: Main Ready

--- app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/MainActivity.java
+++ app/src/main/java/gr/competition/vodafone/vodafonecontestapp/ui/MainActivity.java
@@ -10,7 +10,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import gr.competition.vodafone.vodafonecontestapp.R;
@@ -28,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
     Button btnRewards;
     @BindView(R.id.ll_recharge_layout)
     LinearLayout rechargeLayout;
+    @BindView(R.id.btn_small_recharge)
+    Button btnSmallRecharge;
+    @BindView(R.id.btn_medium_recharge)
+    Button btnMediumRecharge;
+    @BindView(R.id.btn_large_recharge)
+    Button btnLargeRecharge;
+    @BindView(R.id.cordlayout_main)
+    CoordinatorLayout coordinatorLayout;
+    private SharedPreferences prefs;
+    public static final String RETRIES_KEY = "com.example.app.retries";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +49,10 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         AppDatabase db = AppDatabase.Companion.getInstance(this);
-        int retriesAmount = getTries();
-        Log.d("retries", String.valueOf(retriesAmount));
-
 
         btnPlay.setOnClickListener(v -> {
+
+            int retriesAmount = getTries();
 
             if(retriesAmount==0){
                 rechargeLayout.setVisibility(View.VISIBLE);
@@ -66,18 +78,44 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        btnSmallRecharge.setOnClickListener(v -> {
+            int newRetriesAmount = getTries();
+            newRetriesAmount++;
+            prefs.edit().putInt(RETRIES_KEY, newRetriesAmount).apply();
+            success(newRetriesAmount);
+        });
+        btnMediumRecharge.setOnClickListener(v -> {
+            int newRetriesAmount = getTries();
+            newRetriesAmount+=3;
+            prefs.edit().putInt(RETRIES_KEY, newRetriesAmount).apply();
+            success(newRetriesAmount);
+        });
+        btnLargeRecharge.setOnClickListener(v -> {
+            int newRetriesAmount = getTries();
+            newRetriesAmount+=7;
+            prefs.edit().putInt(RETRIES_KEY, newRetriesAmount).apply();
+            success(newRetriesAmount);
+        });
+
+    }
+
+    private void success(int newRetriesAmount) {
+
+        rechargeLayout.setVisibility(View.GONE);
+        Snackbar.make(coordinatorLayout,
+                String.format(getString(R.string.recharge_text),newRetriesAmount),
+                Snackbar.LENGTH_LONG).show();
+
     }
 
     private int getTries() {
 
-        SharedPreferences prefs = this.getSharedPreferences(
+        prefs = this.getSharedPreferences(
                 getPackageName(), Context.MODE_PRIVATE);
 
-
-        String retriesKey = "com.example.app.retries";
-
         // use a default value when retries is empty
-        return prefs.getInt(retriesKey, 0);
+        Log.d("retries", String.valueOf(prefs.getInt(RETRIES_KEY, 0)));
+        return prefs.getInt(RETRIES_KEY, 0);
 
     }
 }
